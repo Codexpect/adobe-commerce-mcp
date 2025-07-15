78b3cd7fece930d6dc31490faba67dab6519e83e
Patryk Walus <patrykwal12@gmail.com>
2025-07-15 11:44:00 +0200

    Cursor rules and refactor

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { initAdobeCommerceClient } from "./adobe/adobe-commerce-client.js";
-import { registerCategoriesTools } from "./tool/tool-for-categories.js";
-import { registerCmsBlockTool as registerCmsBlockTools, registerCmsPageTool as registerCmsPageTools } from "./tool/tool-for-cms.js";
-import { registerCustomerTool as registerCustomerTools } from "./tool/tool-for-customers.js";
-import { registerOrderTool as registerOrderTools } from "./tool/tool-for-orders.js";
-import { registerProductAttributesTools } from "./tool/tool-for-products-attributes.js";
-import { registerProductTools } from "./tool/tool-for-products.js";
+import { registerCategoriesTools } from "./tools/tool-for-categories.js";
+import { registerCmsBlockTool as registerCmsBlockTools, registerCmsPageTool as registerCmsPageTools } from "./tools/tool-for-cms.js";
+import { registerCustomerTool as registerCustomerTools } from "./tools/tool-for-customers.js";
+import { registerOrderTool as registerOrderTools } from "./tools/tool-for-orders.js";
+import { registerProductAttributesTools } from "./tools/tool-for-products-attributes.js";
+import { registerProductTools } from "./tools/tool-for-products.js";
 
 const server = new McpServer({
   name: "adobe-commerce",
